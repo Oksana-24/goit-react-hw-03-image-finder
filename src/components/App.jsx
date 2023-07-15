66dfde75e6e53c6ef1527aa544f0947de0639783
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-07-15 15:35:28 +0200

    add new component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Button } from './Button/Button';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import { Loader } from './Loader/Loader';
+import { Modal } from './Modal/Modal';
+import Searchbar from './Searchbar/Searchbar';
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <Searchbar />
+        <ImageGallery />
+        <ImageGalleryItem />
+        <Loader />
+        <Button />
+        <Modal></Modal>
+      </>
+    );
+  }
+}
+
+export default App;
