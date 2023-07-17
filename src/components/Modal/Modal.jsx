1b0d4e8550dc0d729ce6d8ef89c295e1a85f0e67
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-07-17 19:34:13 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,3 +1,41 @@
-export const Modal = () => {
-  return <div>Modal</div>;
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
+import css from './Modal.module.css';
+import PropTypes from 'prop-types';
+
+const modalRoot = document.querySelector('#modal-root');
+class Modal extends Component {
+  state = {};
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onClose();
+    }
+  };
+  render() {
+    return createPortal(
+      <div className={css.Overlay} onClick={this.handleBackdropClick}>
+        <div className={css.Modal}>{this.props.children}</div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+Modal.propTypes = {
+  modalImg: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
 };
+export default Modal;
