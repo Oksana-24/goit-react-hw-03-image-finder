b38d9bffc333bc7697fea8acee756b08a719b705
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-07-17 19:34:03 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,3 +1,27 @@
-export const ImageGallery = () => {
-  return <div>ImageGallery</div>;
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+import css from './ImageGallery.module.css';
+import PropTypes from 'prop-types';
+
+export const ImageGallery = ({ images, openModal, modalUrl }) => {
+  return (
+    console.log('images', images),
+    (
+      <ul className={css.ImageGallery}>
+        {images?.length > 0 &&
+          images.map(image => (
+            <ImageGalleryItem
+              key={image.id}
+              image={image}
+              openModal={openModal}
+              getModalUrl={modalUrl}
+            />
+          ))}
+      </ul>
+    )
+  );
+};
+ImageGallery.propTypes = {
+  images: PropTypes.array.isRequired,
+  openModal: PropTypes.func.isRequired,
+  modalUrl: PropTypes.func.isRequired,
 };
