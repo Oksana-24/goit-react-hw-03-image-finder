8f358c9f2bcc956c2b717e32f7de9692aa0bab96
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-07-17 19:34:11 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,15 @@
+import { RotatingLines } from 'react-loader-spinner';
+
 export const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <RotatingLines
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+      />
+    </div>
+  );
 };
